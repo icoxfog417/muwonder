8896a208ed3fd0a37c59c3cab8ff917b9da28c1c
icoxfog417 <icoxfog417@yahoo.co.jp>
2014-08-12 11:09:44 +0900

    delete no need directory

--- out/production/muwonder/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .track import Track
-from .criticize import Criticize, CriticizeType
-from .json_serializable import JsonSerializable
--- out/production/muwonder/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url, include
-from soundcloudapi import views
-
-urlpatterns = patterns(
-    '',
-    url(r'^$', views.index, name="index"),
-    url(r'^/recommends/', views.RecommendApi.list)
-)
